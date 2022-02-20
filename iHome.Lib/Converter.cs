38d200e3f00274c75ea7d7c7ccdd299b5b8e2bad
Andrei Voiculescu <voiculescu.andrei@gmail.com>
2022-02-21 00:21:22 +0200

    .

---
+++ iHome.Lib/Converter.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace iHome.Lib
+{
+    public static class Converter
+    {
+        public static Expression<Func<T, bool>> FuncToExpression<T>(Func<T, bool> f)
+        {
+            return x => f(x);
+        }
+    }
+}
